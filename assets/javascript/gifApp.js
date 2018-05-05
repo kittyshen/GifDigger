16e12335df26b884ebef50d47af735d7334e94f4
Kitty <kittykumas@gmail.com>
2018-05-05 01:41:03 -0700

    added a form to let user input data; adjust css to accormadate mobile devices; apple safari can ajust img by max-width; max-block-size doesn't work; filter query response of img by specify their width and w/h ratio to make layout looks a bit more organized

--- assets/javascript/gifApp.js
+++ assets/javascript/gifApp.js
@@ -14,7 +14,7 @@ Step10 capture the form input value from the user input, push the value of strin
 Step11 when user click their own button,start ajax call according to user's input button
 */
 
-var gameArray = ["Beyond Two Souls", "Dark Souls 2", "Monster Hunter: World", "Detroit: Become Human", "Mass Effect 3", "Destiny 2"];
+var gameArray = ["Beyond Two Souls", "Dark Souls 3", "Monster Hunter: World", "Detroit: Become Human", "Mass Effect 3", "Destiny 2"];
 
 //render all the bottons according to the game array data
 var buttonHooker = $("#buttonGroup");  // create a variable to hook all buttons ad future user input append
@@ -40,29 +40,35 @@ function renderUserButton(string){       // create a function to render user inp
 // renderUserButton("New game");  // testing 
 
 function renderImg(obj){
-    $("#gifContainer").html("");
+    $("#gifContainer").html("");  // empty the container for new content;
     for(var i =0; i<obj.data.length; i++ ){
         // console.log(obj.data[i].images.original_still.url);
-        var newDiv =$("<div>").attr("class","imgWrap jumbotron col-md-3 col-sm-4 col-xs-6");
-        var newImg = $("<img>").attr("src", obj.data[i].images.original_still.url);
-        newImg.attr("class","images");
-        newImg.attr("data-still",obj.data[i].images.original.url);
-        newImg.attr("data-animate",obj.data[i].images.original_still.url);
-        newImg.attr("data-state","still");
-        var newP = $("<p>").text(obj.data[i].title);
-        newP.attr("class","text-center");
-        newP.append("<br> Rating: "+obj.data[i].rating);
-        newDiv.append(newImg,newP);
-        $("#gifContainer").append(newDiv);
+        var imgRatio = obj.data[i].images.original_still.width/obj.data[i].images.original_still.height;
+        console.log(imgRatio);
+        // adding this to filter out the image that has ratio not fit the layout
+        if(imgRatio<2.3&& imgRatio>1.4 && obj.data[i].images.original_still.width >200){    
+            console.log("right size");
+            var newDiv =$("<div>").attr("class","imgWrap jumbotron col-md-3 col-sm-4 col-xs-6");
+            var newImg = $("<img>").attr("src", obj.data[i].images.original_still.url);
+            newImg.attr("class","images");
+            newImg.attr("data-still",obj.data[i].images.original.url);
+            newImg.attr("data-animate",obj.data[i].images.original_still.url);
+            newImg.attr("data-state","still");
+            var newP = $("<p>").text(obj.data[i].title);
+            newP.attr("class","text-center");
+            newP.append("<br> Rating: "+obj.data[i].rating);
+            newDiv.append(newImg,newP);
+            $("#gifContainer").append(newDiv);
+        }
     } 
 }
 
 //define a variable to capture user click and store button's value into the var
 var currentQueryVar;
-$(".buttons").on("click", function(){
+$(document).on("click", ".buttons", function(){
     currentQueryVar = $(this).val();
     console.log(currentQueryVar);
-    var currentURL = "https://api.giphy.com/v1/gifs/search?q=" + currentQueryVar + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var currentURL = "https://api.giphy.com/v1/gifs/search?q=" + currentQueryVar +"&api_key=dc6zaTOxFJmzC&limit=17";
 
     $.ajax({
         url:currentURL,
@@ -77,7 +83,7 @@ $(".buttons").on("click", function(){
 });
 
 // render first page with some default content
-var defaultURL = "https://api.giphy.com/v1/gifs/search?q=" + gameArray[0] + "&api_key=dc6zaTOxFJmzC&limit=10"
+var defaultURL = "https://api.giphy.com/v1/gifs/search?q=" + gameArray[2] + "&api_key=dc6zaTOxFJmzC&limit=15"
 $.ajax({
     url:defaultURL,
     method: "GET"
@@ -99,91 +105,13 @@ $(document).on("click",".images",function(){
 
 });
 
-
-
-
-
-// var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
-
-// // displayMovieInfo function re-renders the HTML to display the appropriate content
-// function displayMovieInfo() {
-
-//   var movie = $(this).attr("data-name");
-//   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-//   // Creates AJAX call for the specific movie button being clicked
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     // Creates a div to hold the movie
-//     var movieDisplay = $("<div>");
-//       movieDisplay.attr("id","movieDisplay");
-//     // movieDisplay.text("fdsjhfks");
-    
-//     // Retrieves the Rating Data
-//     console.log(response);
-
-//     // Creates an element to have the rating displayed
-//     var newRating = $("<p>");
-//     newRating.text("Rating: "+response.Rated);
-//     movieDisplay.append(newRating);
-//     var newYear = $("<p>");
-//     newYear.text("Year: "+response.Year);
-//     movieDisplay.append(newYear);
-//     var newPlot = $("<p>");
-//     newPlot.text("Plot: "+response.Plot);
-//     movieDisplay.append(newPlot);
-//     var newImg = $("<img>");
-//     newImg.attr("src",response.Poster);
-//     // newImg.attr("src","https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg");
-//     movieDisplay.append(newImg);
-
-//     // $("h1").append(movieDisplay);
-//     $("#movieDisplay").empty();
-//     $("#buttons-view").prepend(movieDisplay);
-//   });
-
-// }
-
-// // Function for displaying movie data
-// function renderButtons() {
-
-//   // Deletes the movies prior to adding new movies
-//   // (this is necessary otherwise you will have repeat buttons)
-//   $("#buttons-view").html("");
-//   // Loops through the array of movies
-//   for (var i = 0; i < movies.length; i++) {
-
-//     // Then dynamicaly generates buttons for each movie in the array
-//     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-//     var a = $("<button>");
-//     // Adds a class of movie to our button
-//     a.addClass("movie");
-//     // Added a data-attribute
-//     a.attr("data-name", movies[i]);
-//     // Provided the initial button text
-//     a.text(movies[i]);
-//     // Added the button to the buttons-view div
-//     $("#buttons-view").append(a);
-//   }
-// }
-
-// // This function handles events where the add movie button is clicked
-// $("#add-movie").on("click", function(event) {
-//   event.preventDefault();
-//   // This line of code will grab the input from the textbox
-//   var movie = $("#movie-input").val().trim();
-
-//   // The movie from the textbox is then added to our array
-//   movies.push(movie);
-
-//   // Calling renderButtons which handles the processing of our movie array
-//   renderButtons();
-// });
-
-// // Adding click event listeners to all elements with a class of "movie"
-// $(document).on("click", ".movie", displayMovieInfo);
-
-// // Calling the renderButtons function to display the intial buttons
-// renderButtons();
+$("#add-game").on("click", function(event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var game = $("#game-input").val().trim();
+    // The game from the textbox is then added to gameArray
+    gameArray.push(game);
+  
+    //Render the new Button
+    renderUserButton(game);
+});
